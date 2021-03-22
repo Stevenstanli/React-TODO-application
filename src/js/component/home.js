e5c445073669b77fa370164333b953aa72da8526
Gerald2021 <506geraldaguilar@gmail.com>
2021-03-22 07:23:55 +0000

    commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,69 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import shortid from "shortid";
 
 //create your first component
 export function Home() {
+	const [todo, setTodo] = useState("");
+	const [arrayTodos, setArrayTodos] = useState([]);
+
+	let pendingTodo = arrayTodos.length;
+
+	const delet = uniqueId => {
+		let itemDelet = arrayTodos.filter(item => {
+			return item.id !== uniqueId;
+		});
+		setArrayTodos(itemDelet);
+	};
+
+	const addTodo = e => {
+		e.preventDefault();
+		setArrayTodos([
+			...arrayTodos,
+			{
+				id: shortid.generate(),
+				nameTodo: todo
+			}
+		]);
+		setTodo("");
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-light">
+			<div className="row vh-100 align-items-center">
+				<div className="todo bg-white">
+					<header className="header">
+						<h1>todos</h1>
+						<form onSubmit={addTodo}>
+							<input
+								placeholder="What needs to be done?"
+								onChange={e => setTodo(e.target.value)}
+								value={todo}
+							/>
+						</form>
+					</header>
+					<section>
+						{pendingTodo === 0 ? (
+							<p>There are no pending to do list</p>
+						) : (
+							arrayTodos.map(item => (
+								<div key={item.id}>
+									<p>
+										{item.nameTodo}{" "}
+										<i
+											onClick={() => {
+												delet(item.id);
+											}}
+											className="fas fa-trash"></i>
+									</p>
+								</div>
+							))
+						)}
+					</section>
+					<footer className="footer">
+						<p>{pendingTodo} Item Left</p>
+					</footer>
+				</div>
+			</div>
 		</div>
 	);
 }
